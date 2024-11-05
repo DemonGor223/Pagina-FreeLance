7deec1646e0f0202c96c0ad40f8996f33c4dba88
Sebastian Buitrago Perdomo <127266360+DemonGor223@users.noreply.github.com>
2024-11-05 22:16:34 +0000

    cambio 23 (css/html / js)

---
+++ inicio/servicio.js
@@ -0,0 +1,6 @@
+// JavaScript for potential interactive elements can be added here
+// Example: Alert on clicking "Descubrir" button
+
+document.querySelector(".btn").addEventListener("click", function() {
+    alert("Explora los servicios disponibles en nuestra plataforma.");
+});
